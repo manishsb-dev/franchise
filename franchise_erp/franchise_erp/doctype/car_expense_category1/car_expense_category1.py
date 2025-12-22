9ddc0eaba50a2ae1007cde9f3a1e12e7ffa5eaf1
Arpit <arpit.panchal@stackerbee.com>
2025-12-22 12:14:52 +0530

    doctype issue fixed

--- franchise_erp/franchise_erp/doctype/car_expense_category1/car_expense_category1.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Franchise Erp and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class CarExpenseCategory1(Document):
-	pass
