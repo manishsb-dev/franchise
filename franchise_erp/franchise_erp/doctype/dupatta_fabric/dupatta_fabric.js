70c50b161f532d0f902f68ade11ad0fdc86bc6f8
Arpit <arpit.panchal@stackerbee.com>
2025-12-29 18:28:48 +0530

    doctype migrate

---
+++ franchise_erp/franchise_erp/doctype/dupatta_fabric/dupatta_fabric.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Franchise Erp and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Dupatta Fabric", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ franchise_erp/franchise_erp/doctype/tzu_setting/test_tzu_setting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Franchise Erp and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTZUSetting(FrappeTestCase):
+	pass
