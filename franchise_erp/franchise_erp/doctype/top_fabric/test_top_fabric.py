165549b847ab7f69c55c170169098ad6d8507c30
tupe7121 <mayuri.t@stackerbee.com>
2026-01-02 09:59:45 +0530

    PO Tags Print Format with and Without MRP

--- franchise_erp/franchise_erp/doctype/top_fabric/test_top_fabric.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Franchise Erp and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestTopFabric(FrappeTestCase):
-	pass
